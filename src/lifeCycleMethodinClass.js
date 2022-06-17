46e4b7365cadcb0be7d94abaf7a09514e8c8ede6
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-06-17 17:38:43 +0500

    create app

---
+++ src/lifeCycleMethodinClass.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class MethodsInClass extends Component {
+    render() {
+        return (
+            <div>
+                Text In component
+            </div>
+        )
+    }
+}
