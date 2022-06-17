3932c770416a2c8c053d804bd1aec3d9c7f184e2
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-06-17 17:49:17 +0500

    components

--- src/lifeCycleMethodinClass.js
+++ src/lifeCycleMethodinClass.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class MethodsInClass extends Component {
-    render() {
-        return (
-            <div>
-                Text In component
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+    console.log("constructor");
+  }
+
+  render() {
+    console.log("render");
+    return <div>Text In component</div>;
+  }
 }
