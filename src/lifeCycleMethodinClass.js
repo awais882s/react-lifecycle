f2092a76cc336c699138028bed51552bc5eec759
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-06-17 18:08:45 +0500

    Update lifeCycleMethodinClass.js

--- src/lifeCycleMethodinClass.js
+++ src/lifeCycleMethodinClass.js
@@ -4,10 +4,22 @@ export default class MethodsInClass extends Component {
   constructor(props) {
     super(props);
     console.log("constructor");
+    this.state = {
+      date: new Date().toISOString(),
+    };
   }
-
+  componentWillMount() {
+    console.log("componentWillMount");
+  }
+  componentDidMount() {
+    console.log("componentDidMount");
+    this.setState({
+      date: new Date().toISOString(),
+    });
+  }
   render() {
     console.log("render");
-    return <div>Text In component</div>;
+    const { date } = this.state;
+    return <div>Text In Render component{date}</div>;
   }
 }
