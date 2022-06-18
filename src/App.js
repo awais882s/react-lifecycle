6e50bf2eea85058acc5b4f8f9b4a687262e3c467
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-06-18 18:00:32 +0500

    life cycle method in hook

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import LifeCycleMethodsInClass from "./Components/LifeCycleMethodinClass";
+// import ChildClassComponent from "./Components/ChildClassComponent";
+// import LifeCycleMethodsInClass from "./Components/LifeCycleMethodinClass";
+import LifeCycleMethodsInHook from "./Components/lifeCycleMethodsInHook";
 function App() {
-  return <div>
-    <LifeCycleMethodsInClass/>
-  </div>;
+  return (
+    <div>
+      {/* <LifeCycleMethodsInClass />
+      <ChildClassComponent /> */}
+      <LifeCycleMethodsInHook/>
+    </div>
+  );
 }
 export default App;
---
+++ src/Components/lifeCycleMethodsInHook.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function LifeCycleMethodsInHook() {
+  return (
+    <div>lifeCycleMethodsInHook</div>
+  )
+}
