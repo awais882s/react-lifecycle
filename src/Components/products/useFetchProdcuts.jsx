1f9115ec1692960fb68d1e9b36a8841bc13cbd64
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-06-19 10:29:32 +0500

    axios library install
    Co-Authored-By: Muhammad Bilal Iqbal <98903105+bilal882s@users.noreply.github.com>

--- src/Components/products/useFetchProdcuts.jsx
+++ src/Components/products/useFetchProdcuts.jsx
@@ -1,19 +1,36 @@
+import axios from 'axios'
 import React, { useState, useEffect } from 'react'
 // import { apiFetchProducts } from './api';
 export default function useFetchBoard() {
     // logics
     const [products, setProducts] = useState([])
     // end of logic
-    async function apiFetchProducts() {
-        let productsRes = await fetch('https://fakestoreapi.com/products')
-        let products = await productsRes.json()
-        setProducts(products);
-        console.log("Products", products);
-    }
+
+    // using fetch built js network handler
+    // async function apiFetchProducts() {
+    //     let productsRes = await fetch('https://fakestoreapi.com/products')
+    //     let products = await productsRes.json()
+    //     setProducts(products);
+    //     console.log("Products", products);
+    // }
+
     useEffect(() => {
         apiFetchProducts()
-
     }, [])
+
+
+    // using fetch built js network handler
+    async function apiFetchProducts() {
+        try {
+            let products = await axios.get('/https://fakestoreapi.com/products')
+            setProducts(products);
+        }
+        catch (error) {
+            console.log("Erros:", error);
+
+        }
+        // console.log("Products", products);
+    }
 
     // logics return
     return (
